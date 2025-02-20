3ec61579069612b3c3253a6bc6e1f2698673349d
MohammedTayibulla <mohammed@dataseedtech.com>
2025-02-20 13:30:14 +0530

    updated sync

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,24 +1,22 @@
-require('dotenv').config();
-
+require("dotenv").config();
 module.exports = {
   development: {
-    username: 'postgres',
-    password: '1234',
-    database: 'nium-test',
-    host: 'localhost', // Or the IP address if using external Docker networking
-    dialect: 'postgres',
+    username: "postgres",
+    password: "1234",
+    database: "nium-2",
+    host: "localhost",
+    dialect: "postgres", // Ensure this is here
     pool: {
       max: Number(process.env.PGMAX) || 5,
       idle: Number(process.env.PGIDLETIMEOUT) || 10000,
     },
   },
   test: {
-    username: process.env.PGUSER, // Match with .env
+    username: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
     host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    dialect: 'postgres',
+    dialect: "postgres", // Ensure this is here as well
     pool: {
       max: Number(process.env.PGMAX) || 5,
       idle: Number(process.env.PGIDLETIMEOUT) || 10000,
@@ -29,11 +27,45 @@ module.exports = {
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
     host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    dialect: 'postgres',
+    dialect: "postgres", // And here
     pool: {
       max: Number(process.env.PGMAX) || 5,
       idle: Number(process.env.PGIDLETIMEOUT) || 10000,
     },
   },
 };
+
+// module.exports = {
+//   development: {
+//     username: 'postgres',
+//     password: '1234',
+//     database: 'nium-2',
+//     host: 'localhost', // Or the IP address if using external Docker networking
+//     pool: {
+//       max: Number(process.env.PGMAX) || 5,
+//       idle: Number(process.env.PGIDLETIMEOUT) || 10000,
+//     },
+//   },
+//   test: {
+//     username: process.env.PGUSER, // Match with .env
+//     password: process.env.PGPASSWORD,
+//     database: process.env.PGDATABASE,
+//     host: process.env.PGHOST,
+//     port: process.env.PGPORT,
+//     pool: {
+//       max: Number(process.env.PGMAX) || 5,
+//       idle: Number(process.env.PGIDLETIMEOUT) || 10000,
+//     },
+//   },
+//   production: {
+//     username: process.env.PGUSER,
+//     password: process.env.PGPASSWORD,
+//     database: process.env.PGDATABASE,
+//     host: process.env.PGHOST,
+//     port: process.env.PGPORT,
+//     pool: {
+//       max: Number(process.env.PGMAX) || 5,
+//       idle: Number(process.env.PGIDLETIMEOUT) || 10000,
+//     },
+//   },
+// };
--- src/database/providers/database.provider.ts
+++ src/database/providers/database.provider.ts
@@ -1,23 +1,24 @@
 //database.provider.ts
-import { Sequelize } from 'sequelize-typescript';
-import { models } from '../models';
-import { ConfigService } from '@nestjs/config';
-import { LoggerService } from '../../shared/services/logger/logger.service';
-import { NODE_ENV_VALUES } from '../../constants/general.constants';
+import { Sequelize } from "sequelize-typescript";
+import { models } from "../models";
+import { ConfigService } from "@nestjs/config";
+import { LoggerService } from "../../shared/services/logger/logger.service";
+import { NODE_ENV_VALUES } from "../../constants/general.constants";
 
 export const databaseProviders = [
   {
-    provide: 'SEQUELIZE',
+    provide: "SEQUELIZE",
     useFactory: async (config: ConfigService, logger: LoggerService) => {
       const sequelize = new Sequelize({
-        host: config.get('PGHOST'),
-        port: config.get('PGPORT'),
-        database: config.get('PGDATABASE'),
-        dialect: 'postgres',
-        username: config.get('PGUSER'),
-        password: config.get('PGPASSWORD'),
+        host: config.get("PGHOST"),
+        port: config.get("PGPORT"),
+        database: config.get("PGDATABASE"),
+        dialect: "postgres",
+        username: config.get("PGUSER"),
+        password: config.get("PGPASSWORD"),
         logging: (msg) => {
-          if (config.get('NODE_ENV') === NODE_ENV_VALUES.DEVELOPMENT) {
+          if (config.get("NODE_ENV") === NODE_ENV_VALUES.DEVELOPMENT) {
+            console.log("dev");
             logger.info(msg);
           }
         },
@@ -27,50 +28,52 @@ export const databaseProviders = [
         },
         dialectOptions: {
           connectTimeout: 3000,
-          application_name: config.get('SERVICE_NAME'),
+          application_name: config.get("SERVICE_NAME"),
         },
         pool: {
-          max: parseInt(config.get('PGMAX'), 10) || 5,
+          max: parseInt(config.get("PGMAX"), 10) || 5,
           min: 1,
-          idle: config.get('PGIDLETIMEOUT'),
+          idle: config.get("PGIDLETIMEOUT"),
         },
       });
-      console.log('Loaded Models:', models);
+      console.log("Loaded Models:", models);
       await sequelize.addModels(models);
-      
-      // Add models to sequelize instance
-      // await sequelize.addModels(models);
 
       // Authenticate the connection
       await sequelize
         .authenticate()
         .then(() => {
-          logger.info('Postgres DB - Connection successful');
+          logger.info("Postgres DB - Connection successful");
         })
         .catch((error) => {
           logger.error(
-            `DB - Connection failed. Error: ${error} ${config.get('PGHOST')} ${config.get('PGPORT')} ${config.get('PGUSER')} ${config.get('PGDATABASE')}`,
-            error.message || error,
+            `DB - Connection failed. Error: ${error} ${config.get(
+              "PGHOST"
+            )} ${config.get("PGPORT")} ${config.get("PGUSER")} ${config.get(
+              "PGDATABASE"
+            )}`,
+            error.message || error
           );
           process.exit(1);
         });
 
+
       // // Conditionally sync models based on environment
-      // if (config.get('NODE_ENV') === NODE_ENV_VALUES.DEVELOPMENT) {
-        
+      // if (config.get("NODE_ENV") === NODE_ENV_VALUES.DEVELOPMENT) {
       //   // In development: Sync models and adjust tables if necessary
       //   await sequelize.sync({
-      //     force: false,  // Don't drop existing tables
-      //     alter: true,   // Automatically adjust tables to match models
+      //     force: false, // Don't drop existing tables
+      //     alter: true, // Automatically adjust tables to match models
+      //     logging: console.log,
       //   });
-      //   logger.info('Database synced successfully in development');
+      //   logger.info("Database synced successfully in development");
       // } else {
       //   // In production: Don't sync automatically (for safety)
       //   await sequelize.sync({
-      //     force: false,  // Don't drop tables in production
-      //     alter: false,  // Don't alter existing tables
+      //     force: false, // Don't drop tables in production
+      //     alter: false, // Don't alter existing tables
       //   });
-      //   logger.info('Database sync skipped in production');
+      //   logger.info("Database sync skipped in production");
       // }
 
       return sequelize;
