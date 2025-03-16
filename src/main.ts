8586ef46064c178a8be52a0a79d4135efc762c1f
MohammedTayibulla <mohammed@dataseedtech.com>
2025-03-16 20:54:52 +0530

    masked urls

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ import { LoggerService } from "./shared/services/logger/logger.service";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import * as morgan from "morgan";
 import helmet from "helmet";
-import { json } from 'express'; // Import express json middleware
+import { json } from "express"; // Import express json middleware
 import { ConfigService } from "@nestjs/config";
+import * as httpProxy from "http-proxy-middleware";
+import { Request, Response, NextFunction } from "express";
+import { createProxyMiddleware, Options } from "http-proxy-middleware";
 const contextService = require("request-context");
 
 async function bootstrap() {
@@ -33,7 +36,7 @@ async function bootstrap() {
       "https://13.201.102.229",
       "http://nium.thestorywallcafe.com",
       "https://nium.thestorywallcafe.com",
-      "https://nium-forex-agent-portal.vercel.app"
+      "https://nium-forex-agent-portal.vercel.app",
     ], // Allow frontend on localhost
     credentials: true,
     methods: ["GET", "POST", "PUT", "OPTIONS"], // Explicitly allow methods
@@ -43,7 +46,10 @@ async function bootstrap() {
   // ✅ Handle OPTIONS requests explicitly
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
     if (req.method === "OPTIONS") {
@@ -54,10 +60,56 @@ async function bootstrap() {
   });
   app.use(helmet());
   // Increase the JSON body size limit to 1MB (or adjust as needed)
-  app.use(json({ limit: '5mb' })); // 5mb = 5120 * 1024 bytes
+  app.use(json({ limit: "5mb" })); // 5mb = 5120 * 1024 bytes
   app.setGlobalPrefix("v1/api");
   app.use(contextService.middleware("request"));
 
+  // Proxy Routes
+  // app.use('/tayib-jet.vercel.app/*', createProxyMiddleware({
+  //   target: 'https://tayib-jet.vercel.app',
+  //   changeOrigin: true,
+  //   pathRewrite: (path, req) => {
+  //     return path.replace('/tayib-jet.vercel.app', '');
+  //   },
+  //   onProxyReq: (proxyReq, req, res) => {
+  //     // Ensure the origin header is set correctly
+  //     proxyReq.setHeader('Origin', 'https://tayib-jet.vercel.app');
+  //   }
+  // } as any)); // Cast to `any` to bypass TypeScript type issue
+
+  // Proxy for v_kyc_link
+
+ const proxyOptions = {
+  target: 'https://capture.kyc.idfy.com', // Target API
+  changeOrigin: true, // Correct origin header handling
+  pathRewrite: {
+    '^/captures': '/v2/captures',  // Rewrite '/captures' to '/v2/captures'
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    // Optional: Set additional headers if needed
+    proxyReq.setHeader('Origin', 'https://capture.kyc.idfy.com');
+  },
+};
+  // Apply proxy middleware for '/captures' route
+  app.use('/captures', createProxyMiddleware(proxyOptions));
+
+ 
+
+  const proxyOptionsEsign = {
+    target: 'https://app1.leegality.com', // Target e-sign service
+ 
+    changeOrigin: true, // Correct origin header handling
+    pathRewrite: {
+      '^/sign': '',  // Rewrite '/captures' to '/v2/captures'
+    },
+    onProxyReq: (proxyReq, req, res) => {
+      // Optional: Set additional headers if needed
+      proxyReq.setHeader('Origin', 'https://app1.leegality.com');
+    },
+  };
+    // Apply proxy middleware for '/captures' route
+    app.use('/sign/*', createProxyMiddleware(proxyOptionsEsign));
+  
   // listen for kill signal
   // app.enableShutdownHooks();
   // Global Unhandled Promise Catcher
@@ -90,7 +142,7 @@ async function bootstrap() {
       filter: true, // ✅ Enables search for controllers & endpoints
     },
   });
-  
+
   const port = config.get<number>("PORT") || 3002; // Default to 3002 if undefined
 
   await app.listen(port);
