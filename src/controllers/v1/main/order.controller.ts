ba3e01527c0d74743184c2c6b52a810ae6a2089e
suraj-dataseedtech <suraj@dataseedtech.com>
2025-03-16 11:35:13 +0530

    feat: update checker id for respective order to update assign or select order by checker take action

--- src/controllers/v1/main/order.controller.ts
+++ src/controllers/v1/main/order.controller.ts
@@ -1,7 +1,22 @@
 // orders.controller.ts
-import { Controller, Post, Get, Put, Delete, Body, Param, Headers, BadRequestException, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Body,
+  Param,
+  Headers,
+  BadRequestException,
+  ValidationPipe,
+} from '@nestjs/common';
 import { OrdersService } from '../../../services/v1/order/order.service';
-import { CreateOrderDto,UpdateOrderDto } from '../../../dto/order.dto';
+import {
+  CreateOrderDto,
+  UpdateOrderDto,
+  UpdateCheckerDto,
+} from '../../../dto/order.dto';
 import { ApiTags, ApiResponse, ApiHeader } from '@nestjs/swagger';
 import * as opentracing from 'opentracing';
 
@@ -11,16 +26,21 @@ export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
   @Post()
   async createOrder(
-    @Headers('partner-id-00eb04d0-646c-41d5-a69e-197b2b504f01') partnerId: string,
+    @Headers('partner-id-00eb04d0-646c-41d5-a69e-197b2b504f01')
+    partnerId: string,
     @Headers('api-key-c1c9773f-49be-4f31-b37a-a853dc2b2981') apiKey: string,
     @Body() createOrderDto: CreateOrderDto,
   ) {
     const tracer = opentracing.globalTracer();
     const span = tracer.startSpan('create-order-controller');
-    
+
     try {
       await this.ordersService.validatePartnerHeaders(partnerId, apiKey);
-      const order = await this.ordersService.createOrder(span, createOrderDto, partnerId);
+      const order = await this.ordersService.createOrder(
+        span,
+        createOrderDto,
+        partnerId,
+      );
       return {
         success: true,
         data: order,
@@ -36,11 +56,18 @@ export class OrdersController {
   @ApiResponse({ status: 200, description: 'Order updated successfully' })
   async updateOrder(
     @Param('orderId') orderId: string,
-    @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })) updateOrderDto: UpdateOrderDto,
+    @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+    updateOrderDto: UpdateOrderDto,
   ) {
-    const span = opentracing.globalTracer().startSpan('update-order-controller');
+    const span = opentracing
+      .globalTracer()
+      .startSpan('update-order-controller');
     try {
-      return await this.ordersService.updateOrder(span, orderId, updateOrderDto);
+      return await this.ordersService.updateOrder(
+        span,
+        orderId,
+        updateOrderDto,
+      );
     } finally {
       span.finish();
     }
@@ -49,7 +76,9 @@ export class OrdersController {
   @Get()
   @ApiResponse({ status: 200, description: 'List of orders' })
   async findAll() {
-    const span = opentracing.globalTracer().startSpan('find-all-orders-controller');
+    const span = opentracing
+      .globalTracer()
+      .startSpan('find-all-orders-controller');
     try {
       return await this.ordersService.findAll(span);
     } finally {
@@ -60,7 +89,9 @@ export class OrdersController {
   @Get(':orderId')
   @ApiResponse({ status: 200, description: 'Order details' })
   async findOne(@Param('orderId') orderId: string) {
-    const span = opentracing.globalTracer().startSpan('find-one-order-controller');
+    const span = opentracing
+      .globalTracer()
+      .startSpan('find-one-order-controller');
     try {
       return await this.ordersService.findOne(span, orderId);
     } finally {
@@ -68,17 +99,30 @@ export class OrdersController {
     }
   }
 
- 
-
   @Delete(':orderId')
   @ApiResponse({ status: 204, description: 'Order deleted successfully' })
   async deleteOrder(@Param('orderId') orderId: string) {
-    const span = opentracing.globalTracer().startSpan('delete-order-controller');
+    const span = opentracing
+      .globalTracer()
+      .startSpan('delete-order-controller');
     try {
       await this.ordersService.deleteOrder(span, orderId);
       return { message: 'Order deleted successfully' };
     } finally {
       span.finish();
     }
+  }
+
+  @Put('update-checker')
+  @ApiResponse({ status: 200, description: 'Checker ID updated successfully' })
+  @ApiResponse({
+    status: 404,
+    description: 'Checker ID or Order IDs not found',
+  })
+  async updateChecker(
+    @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: false }))
+    updateCheckerDto: UpdateCheckerDto,
+  ) {
+    return this.ordersService.updateChecker(updateCheckerDto);
   }
 }
--- src/dto/order.dto.ts
+++ src/dto/order.dto.ts
@@ -1,183 +1,179 @@
 // create-order.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
-import { 
-  IsString, 
-  IsBoolean, 
-  IsEmail, 
-  IsUUID, 
-  IsOptional, 
+import {
+  IsString,
+  IsBoolean,
+  IsEmail,
+  IsUUID,
+  IsOptional,
   IsDateString,
   IsArray,
-  IsObject, 
-  Matches, 
-  IsPhoneNumber 
+  IsObject,
+  Matches,
+  IsPhoneNumber,
 } from 'class-validator';
 
-
 export class CreateOrderDto {
-  @ApiProperty({ 
-    type: String, 
-    description: 'Partner Order ID', 
-    example: 'BMFORDERID4321' 
+  @ApiProperty({
+    type: String,
+    description: 'Partner Order ID',
+    example: 'BMFORDERID4321',
   })
   @IsString()
   partner_order_id: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Transaction Type ID', 
-    example: 'a8712d83154b960b9d3803d30b1112cam84dhj1k' 
+  @ApiProperty({
+    type: String,
+    description: 'Transaction Type ID',
+    example: 'a8712d83154b960b9d3803d30b1112cam84dhj1k',
   })
   @IsUUID()
   @IsOptional()
   transaction_type_id: string;
 
-  
-  @ApiProperty({ 
-    type: String, 
-    description: 'Purpose Type ID', 
-    example: '378dcac6a3a4c406cc11e112b91a99e8m84dbjsa' 
+  @ApiProperty({
+    type: String,
+    description: 'Purpose Type ID',
+    example: '378dcac6a3a4c406cc11e112b91a99e8m84dbjsa',
   })
   @IsUUID()
   @IsOptional()
   purpose_type_id: string;
 
-  @ApiProperty({ 
-    type: Boolean, 
-    description: 'Indicates if e-signature is required', 
-    example: true 
+  @ApiProperty({
+    type: Boolean,
+    description: 'Indicates if e-signature is required',
+    example: true,
   })
   @IsBoolean()
   is_e_sign_required: boolean;
 
-  @ApiProperty({ 
-    type: Boolean, 
-    description: 'Indicates if V-KYC is required', 
-    example: true 
+  @ApiProperty({
+    type: Boolean,
+    description: 'Indicates if V-KYC is required',
+    example: true,
   })
   @IsBoolean()
   is_v_kyc_required: boolean;
 
-
-  @ApiProperty({ 
-    type: String, 
-    description: 'Customer Name', 
-    example: 'John Doe' 
+  @ApiProperty({
+    type: String,
+    description: 'Customer Name',
+    example: 'John Doe',
   })
   @IsString()
   customer_name: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Customer Email', 
-    example: 'john@gmail.com' 
+  @ApiProperty({
+    type: String,
+    description: 'Customer Email',
+    example: 'john@gmail.com',
   })
   @IsEmail()
   customer_email: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Customer Phone', 
-    example: '9912345678' 
+  @ApiProperty({
+    type: String,
+    description: 'Customer Phone',
+    example: '9912345678',
   })
   @IsPhoneNumber('IN')
   customer_phone: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Customer PAN', 
-    example: 'ACTPAN123' 
+  @ApiProperty({
+    type: String,
+    description: 'Customer PAN',
+    example: 'ACTPAN123',
   })
   @Matches(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/, {
     message: 'Invalid PAN format',
   })
   customer_pan: string;
-
 }
 
 export class CreateOrderDto2 {
-  @ApiProperty({ 
-    type: String, 
-    description: 'Order ID', 
-    uniqueItems: true, 
-    example: 'BMFORDERID001' 
+  @ApiProperty({
+    type: String,
+    description: 'Order ID',
+    uniqueItems: true,
+    example: 'BMFORDERID001',
   })
   @IsString()
   order_id: string;
 
-  @ApiProperty({ 
-    type: Boolean, 
-    description: 'Indicates if e-signature is required', 
-    example: true 
+  @ApiProperty({
+    type: Boolean,
+    description: 'Indicates if e-signature is required',
+    example: true,
   })
   @IsBoolean()
   is_e_sign_required: boolean;
 
-  @ApiProperty({ 
-    type: Boolean, 
-    description: 'Indicates if V-KYC is required', 
-    example: false 
+  @ApiProperty({
+    type: Boolean,
+    description: 'Indicates if V-KYC is required',
+    example: false,
   })
   @IsBoolean()
   is_v_kyc_required: boolean;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Customer Name', 
-    example: 'Mohammed Tayibulla' 
+  @ApiProperty({
+    type: String,
+    description: 'Customer Name',
+    example: 'Mohammed Tayibulla',
   })
   @IsString()
   customer_name: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Customer Email', 
-    example: 'mohammed@dataseedtech.com' 
+  @ApiProperty({
+    type: String,
+    description: 'Customer Email',
+    example: 'mohammed@dataseedtech.com',
   })
   @IsEmail()
   customer_email: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Customer Phone', 
-    example: '8550895486' 
+  @ApiProperty({
+    type: String,
+    description: 'Customer Phone',
+    example: '8550895486',
   })
   @IsPhoneNumber('IN')
   customer_phone: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Customer PAN', 
-    example: 'DAIPT0727K' 
+  @ApiProperty({
+    type: String,
+    description: 'Customer PAN',
+    example: 'DAIPT0727K',
   })
   @Matches(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/, {
     message: 'Invalid PAN format',
   })
   customer_pan: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Created By (Partner ID)', 
-    example: '00eb04d0-646c-41d5-a69e-197b2b504f01', 
-    required: true 
+  @ApiProperty({
+    type: String,
+    description: 'Created By (Partner ID)',
+    example: '00eb04d0-646c-41d5-a69e-197b2b504f01',
+    required: true,
   })
   @IsUUID()
   created_by: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Updated By (Partner ID)', 
-    example: '00eb04d0-646c-41d5-a69e-197b2b504f01', 
-    required: true 
+  @ApiProperty({
+    type: String,
+    description: 'Updated By (Partner ID)',
+    example: '00eb04d0-646c-41d5-a69e-197b2b504f01',
+    required: true,
   })
   @IsUUID()
   updated_by: string;
 
-  @ApiProperty({ 
-    type: String, 
-    description: 'Checker ID (User ID)', 
-    example: '49592f43-c59f-4084-bf3a-79a7ba6f182e', 
-    required: true 
+  @ApiProperty({
+    type: String,
+    description: 'Checker ID (User ID)',
+    example: '49592f43-c59f-4084-bf3a-79a7ba6f182e',
+    required: true,
   })
   @IsUUID()
   @IsOptional()
@@ -185,9 +181,13 @@ export class CreateOrderDto2 {
 
   @ApiProperty({
     type: Object,
-    description: "Merged document details",
+    description: 'Merged document details',
     required: false,
-    example: { url: "http://example.com/merged.pdf", mimeType: "application/pdf", size: 1024 },
+    example: {
+      url: 'http://example.com/merged.pdf',
+      mimeType: 'application/pdf',
+      size: 1024,
+    },
   })
   @IsOptional()
   @IsObject()
@@ -201,7 +201,7 @@ export class CreateOrderDto2 {
 
   @ApiProperty({
     type: [Object],
-    description: "Array of documents associated with the order",
+    description: 'Array of documents associated with the order',
     required: false,
   })
   @IsOptional()
@@ -210,8 +210,13 @@ export class CreateOrderDto2 {
     purposeId: string;
     document_type_id: string;
     documentName: string;
-    documentUrl: { url: string; mimeType?: string; size?: number; uploadedAt?: string };
-    status?: "pending" | "approved" | "rejected";
+    documentUrl: {
+      url: string;
+      mimeType?: string;
+      size?: number;
+      uploadedAt?: string;
+    };
+    status?: 'pending' | 'approved' | 'rejected';
     documentExpiry?: string;
     isDocFrontImage?: boolean;
     isDocBackImage?: boolean;
@@ -220,47 +225,144 @@ export class CreateOrderDto2 {
 }
 
 export class UpdateOrderDto {
-  @ApiProperty({ example: 'BMFORDERID432' }) @IsString() @IsOptional() order_id?: string;
-  @ApiProperty({ example: 'a8712d83154b960b9d3803d30b1112cam84dhj1k' }) @IsString() @IsOptional() transaction_type?: string;
-  @ApiProperty({ example: '378dcac6a3a4c406cc11e112b91a99e8m84dbjsa' }) @IsString() @IsOptional() purpose_type?: string;
-  @ApiProperty({ example: true }) @IsBoolean() @IsOptional() is_e_sign_required?: boolean;
-  @ApiProperty({ example: true }) @IsBoolean() @IsOptional() is_v_kyc_required?: boolean;
-  @ApiProperty({ example: 'Mohammed Tayibulla' }) @IsString() @IsOptional() customer_name?: string;
-  @ApiProperty({ example: 'contact2tayib@gmail.com' }) @IsEmail() @IsOptional() customer_email?: string;
-  @ApiProperty({ example: '8550895486' }) @IsPhoneNumber('IN') @IsOptional() customer_phone?: string;
-  @ApiProperty({ example: 'CAIPT0727K' }) @Matches(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/) @IsOptional() customer_pan?: string;
-  @ApiProperty({ example: 'pending' }) @IsString() @IsOptional() order_status?: string;
+  @ApiProperty({ example: 'BMFORDERID432' })
+  @IsString()
+  @IsOptional()
+  order_id?: string;
+  @ApiProperty({ example: 'a8712d83154b960b9d3803d30b1112cam84dhj1k' })
+  @IsString()
+  @IsOptional()
+  transaction_type?: string;
+  @ApiProperty({ example: '378dcac6a3a4c406cc11e112b91a99e8m84dbjsa' })
+  @IsString()
+  @IsOptional()
+  purpose_type?: string;
+  @ApiProperty({ example: true })
+  @IsBoolean()
+  @IsOptional()
+  is_e_sign_required?: boolean;
+  @ApiProperty({ example: true })
+  @IsBoolean()
+  @IsOptional()
+  is_v_kyc_required?: boolean;
+  @ApiProperty({ example: 'Mohammed Tayibulla' })
+  @IsString()
+  @IsOptional()
+  customer_name?: string;
+  @ApiProperty({ example: 'contact2tayib@gmail.com' })
+  @IsEmail()
+  @IsOptional()
+  customer_email?: string;
+  @ApiProperty({ example: '8550895486' })
+  @IsPhoneNumber('IN')
+  @IsOptional()
+  customer_phone?: string;
+  @ApiProperty({ example: 'CAIPT0727K' })
+  @Matches(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/)
+  @IsOptional()
+  customer_pan?: string;
+  @ApiProperty({ example: 'pending' })
+  @IsString()
+  @IsOptional()
+  order_status?: string;
 
   // E-Sign
-  @ApiProperty({ example: 'Pending' }) @IsString() @IsOptional() e_sign_status?: string;
-  @ApiProperty({ example: 'https://esign-link.com' }) @IsString() @IsOptional() e_sign_link?: string;
-  @ApiProperty({ example: 'active' }) @IsString() @IsOptional() e_sign_link_status?: string;
-  @ApiProperty({ example: '6ae8a7a6-55fa-457b-932f-a4ba271f8eee' }) @IsString() @IsOptional() e_sign_link_request_id?: string;
-  @ApiProperty({ example: '01JP0H1M86CDW8HA4WF7V3X7HA' }) @IsString() @IsOptional() e_sign_link_doc_id?: string; 
-  @ApiProperty({ example: '2025-03-30T12:00:00.000Z' }) @IsDateString() @IsOptional() e_sign_link_expires?: string;
-  @ApiProperty({ example: false }) @IsBoolean() @IsOptional() e_sign_completed_by_customer?: boolean;
-  @ApiProperty({ example: '2025-03-13T08:40:53.328Z' }) @IsDateString() @IsOptional() e_sign_customer_completion_date?: string;
-  @ApiProperty({ example: 'Signed successfully' }) @IsString() @IsOptional() e_sign_doc_comments?: string;
+  @ApiProperty({ example: 'Pending' })
+  @IsString()
+  @IsOptional()
+  e_sign_status?: string;
+  @ApiProperty({ example: 'https://esign-link.com' })
+  @IsString()
+  @IsOptional()
+  e_sign_link?: string;
+  @ApiProperty({ example: 'active' })
+  @IsString()
+  @IsOptional()
+  e_sign_link_status?: string;
+  @ApiProperty({ example: '6ae8a7a6-55fa-457b-932f-a4ba271f8eee' })
+  @IsString()
+  @IsOptional()
+  e_sign_link_request_id?: string;
+  @ApiProperty({ example: '01JP0H1M86CDW8HA4WF7V3X7HA' })
+  @IsString()
+  @IsOptional()
+  e_sign_link_doc_id?: string;
+  @ApiProperty({ example: '2025-03-30T12:00:00.000Z' })
+  @IsDateString()
+  @IsOptional()
+  e_sign_link_expires?: string;
+  @ApiProperty({ example: false })
+  @IsBoolean()
+  @IsOptional()
+  e_sign_completed_by_customer?: boolean;
+  @ApiProperty({ example: '2025-03-13T08:40:53.328Z' })
+  @IsDateString()
+  @IsOptional()
+  e_sign_customer_completion_date?: string;
+  @ApiProperty({ example: 'Signed successfully' })
+  @IsString()
+  @IsOptional()
+  e_sign_doc_comments?: string;
 
   // V-KYC
-  @ApiProperty({ example: 'Pending' }) @IsString() @IsOptional() v_kyc_status?: string;
-  @ApiProperty({ example: 'https://vkyc-link.com' }) @IsString() @IsOptional() v_kyc_link?: string;
-  @ApiProperty({ example: 'active' }) @IsString() @IsOptional() v_kyc_link_status?: string;
-  @ApiProperty({ example: '2025-03-30T12:00:00.000Z' }) @IsDateString() @IsOptional() v_kyc_link_expires?: string;
-  @ApiProperty({ example: false }) @IsBoolean() @IsOptional() v_kyc_completed_by_customer?: boolean;
-  @ApiProperty({ example: '2025-03-13T08:40:53.328Z' }) @IsDateString() @IsOptional() v_kyc_customer_completion_date?: string;
-  @ApiProperty({ example: 'KYC verified' }) @IsString() @IsOptional() v_kyc_comments?: string;
+  @ApiProperty({ example: 'Pending' })
+  @IsString()
+  @IsOptional()
+  v_kyc_status?: string;
+  @ApiProperty({ example: 'https://vkyc-link.com' })
+  @IsString()
+  @IsOptional()
+  v_kyc_link?: string;
+  @ApiProperty({ example: 'active' })
+  @IsString()
+  @IsOptional()
+  v_kyc_link_status?: string;
+  @ApiProperty({ example: '2025-03-30T12:00:00.000Z' })
+  @IsDateString()
+  @IsOptional()
+  v_kyc_link_expires?: string;
+  @ApiProperty({ example: false })
+  @IsBoolean()
+  @IsOptional()
+  v_kyc_completed_by_customer?: boolean;
+  @ApiProperty({ example: '2025-03-13T08:40:53.328Z' })
+  @IsDateString()
+  @IsOptional()
+  v_kyc_customer_completion_date?: string;
+  @ApiProperty({ example: 'KYC verified' })
+  @IsString()
+  @IsOptional()
+  v_kyc_comments?: string;
 
   // Regeneration
-  @ApiProperty({ example: false }) @IsBoolean() @IsOptional() is_esign_regenerated?: boolean;
-  @ApiProperty({ example: { reason: 'expired' } }) @IsOptional() is_esign_regenerated_details?: any;
-  @ApiProperty({ example: false }) @IsBoolean() @IsOptional() is_video_kyc_link_regenerated?: boolean;
-  @ApiProperty({ example: { reason: 'expired' } }) @IsOptional() is_video_kyc_link_regenerated_details?: any;
+  @ApiProperty({ example: false })
+  @IsBoolean()
+  @IsOptional()
+  is_esign_regenerated?: boolean;
+  @ApiProperty({ example: { reason: 'expired' } })
+  @IsOptional()
+  is_esign_regenerated_details?: any;
+  @ApiProperty({ example: false })
+  @IsBoolean()
+  @IsOptional()
+  is_video_kyc_link_regenerated?: boolean;
+  @ApiProperty({ example: { reason: 'expired' } })
+  @IsOptional()
+  is_video_kyc_link_regenerated_details?: any;
 
   // Tracking
-  @ApiProperty({ example: '00eb04d0-646c-41d5-a69e-197b2b504f01' }) @IsUUID() @IsOptional() created_by?: string;
-  @ApiProperty({ example: '00eb04d0-646c-41d5-a69e-197b2b504f01' }) @IsUUID() @IsOptional() updated_by?: string;
-  @ApiProperty({ example: '49592f43-c59f-4084-bf3a-79a7ba6f182e' }) @IsUUID() @IsOptional() checker_id?: string;
+  @ApiProperty({ example: '00eb04d0-646c-41d5-a69e-197b2b504f01' })
+  @IsUUID()
+  @IsOptional()
+  created_by?: string;
+  @ApiProperty({ example: '00eb04d0-646c-41d5-a69e-197b2b504f01' })
+  @IsUUID()
+  @IsOptional()
+  updated_by?: string;
+  @ApiProperty({ example: '49592f43-c59f-4084-bf3a-79a7ba6f182e' })
+  @IsUUID()
+  @IsOptional()
+  checker_id?: string;
 }
 // export class UpdateOrderDto {
 //   @ApiProperty({ type: String, description: 'Order ID', example: 'BMFORDERID432' })
@@ -362,3 +464,22 @@ export class UpdateOrderDto {
 //   @IsUUID()
 //   checker_id: string;
 // }
+
+export class UpdateCheckerDto {
+  @ApiProperty({
+    type: [String],
+    description: 'Array of Order IDs',
+    example: ['912707e7-9b03-4f42-a30d-d1e583f6cd2f'],
+  })
+  @IsArray()
+  @IsUUID('4', { each: true })
+  orderIds: string[];
+
+  @ApiProperty({
+    type: String,
+    description: 'Checker ID (Hashed Key)',
+    example: 'aab26dd990e49d40cf5bc80774ef7e0bm87gffio',
+  })
+  @IsString()
+  checkerId: string; // 🔹 Changed from IsUUID("4") to IsString()
+}
