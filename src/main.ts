068585f5303d088b209ee0a21d3e1bdf25b5080f
kanchanmalan <kanchanmalan@kanchanmalanaukudkar.local>
2025-04-07 18:10:23 +0530

    fix(minor): added

--- src/main.ts
+++ src/main.ts
@@ -69,7 +69,7 @@ async function bootstrap() {
   app.use(helmet());
   // Increase the JSON body size limit to 1MB (or adjust as needed)
   app.use(json({ limit: '5mb' })); // 5mb = 5120 * 1024 bytes
-  app.setGlobalPrefix('/');
+  app.setGlobalPrefix('/api');
   app.use(contextService.middleware('request'));
 
   const proxyOptions = {
